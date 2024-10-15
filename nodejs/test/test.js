3aa2568c32579f4e645e13ee4fab9c31edf9bce9
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2024-10-15 09:57:31 -0300

    creerio test

--- nodejs/test/test.js
+++ nodejs/test/test.js
@@ -1,4 +1,22 @@
 var assert = require('assert');
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+describe('index.html', function () {
+  var html = fs.readFileSync('index.html', 'utf8');
+  var $ = cheerio.load(html);
+
+  it('should have a title', function () {
+    var title = $('title').text();
+    assert.notEqual(title, '', 'Title should not be empty');
+  });
+
+  it('should have a body section', function () {
+    var body = $('body');
+    assert.equal(body.length, 1, 'There should be one body section');
+  });
+});
+
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
